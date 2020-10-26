cf885cf7f03ad20f82a7f36af44a969417ab8fab
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2020-10-25 23:56:40 -0700

    Add files via upload

--- smallSSH.c
+++ smallSSH.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
+#include <stdio.h>  // printf
+#include <sys/types.h> // pid_t, not used in this example
+#include <unistd.h> // getpid, getppid
 
-int main(void)
-{
-  printf("Hello World\n");
+int main(void) {
+  printf("My pid is %d\n", getpid());
+  printf("My parent's pid is %d\n", getppid());
   return 0;
 }
