c324fffe33e1aeb2c9ac2a1588b1965f817949ab
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2020-10-27 02:13:50 -0700

    Add files via upload

--- smallSSH.c
+++ smallSSH.c
@@ -1,9 +1,66 @@
-#include <stdio.h>  // printf
-#include <sys/types.h> // pid_t, not used in this example
-#include <unistd.h> // getpid, getppid
+#include <stdio.h>  // for perror
+#include <stdlib.h>   // for exit
+#include <unistd.h>   // for execv
 
-int main(void) {
-  printf("My pid is %d\n", getpid());
-  printf("My parent's pid is %d\n", getppid());
-  return 0;
+#define MAX_LIMIT 60
+
+
+
+// user input
+char userInput[MAX_LIMIT];
+
+
+FILE *fp;
+
+char expansion[] = "$$";
+
+char comment[] = "#";
+
+// basically throw away the line because it is a comment
+char trash;
+
+char *buffer;
+    size_t bufsize = 32;
+    size_t characters;
+// used to clear the buffer 
+        // and accept the next string 
+ //       fflush(stdin); 
+
+
+
+int main(){
+	
+	 buffer = (char *)malloc(bufsize * sizeof(char));
+//	while(userInput != -5){
+		printf(": ");
+		fgets(userInput, MAX_LIMIT, stdin); 
+   		printf("%s", userInput);	
+//	}
+
+	char *point = strstr(userInput, expansion);
+	
+	if(point != NULL) {
+		printf("%d", getpid());
+	}
+	
+	if((strncmp(comment, userInput, strlen(comment)) == 0)) {
+    	trash = getline(&buffer,&bufsize,stdin);
+	}
+
 }
+//\\char *buffer;
+//    size_t bufsize = 32;
+//    size_t characters;
+//
+//    buffer = (char *)malloc(bufsize * sizeof(char));
+//    if( buffer == NULL)
+//    {
+//        perror("Unable to allocate buffer");
+//        exit(1);
+//    }
+//
+//    printf("Type something: ");
+//    characters = getline(&buffer,&bufsize,stdin);
+//    printf("%zu characters were read.\n",characters);
+//    printf("You typed: '%s'\n",buffer);
+
