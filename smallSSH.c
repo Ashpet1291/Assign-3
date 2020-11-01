6299091980d2a15d20baff38752acab8b1ef3211
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2020-10-31 17:01:28 -0700

    Add files via upload

--- smallSSH.c
+++ smallSSH.c
@@ -190,7 +190,7 @@ int main(){
 		printf("this is comment%s", comment);
 		printf("this s space%sthis is end of space", space);
 		// checking for string and comments
-		if((strncmp(comment, userInput, strlen(comment) == 0) || (strncmp(space, userInput, strlen(space) == 0)) {
+		if(strncmp(comment, userInput, strlen(comment) == 0) || (strncmp(space, userInput, strlen(space) == 0))) {
     	//	trash = getline(&buffer,&bufsize,stdin);
   		//  printf(": ");
 	}
