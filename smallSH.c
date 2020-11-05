21bae0758ac7654d5044517a39c2f93f3cb79d3b
Ashpet1291 <49249566+Ashpet1291@users.noreply.github.com>
2020-11-05 10:36:52 -0800

    Add files via upload

--- smallSH.c
+++ smallSH.c
@@ -131,10 +131,13 @@ void changeDir() {
 	// code in program, if user command is cd followed by file, or file descriptor, try to change the directory
 	// if chdir or fchdir doesn't fail then change directories, else cant change directories
 	// if not ./, then add that and chdir
-	
+	int x =0;
 		
 	if(args == 1) {
-		chdir(getenv("HOME"));
+	x =	chdir(getenv("HOME"));
+		if(x == 0) {
+			printf("change was succesfull \n");
+		}
 	}
 	else {	
 	
@@ -187,7 +190,7 @@ void status(int exitVal) {
 //	printf("this is in the status command");
 //If this command is run before any foreground command is run, then it should simply return the exit status 0.
 //The three built-in shell commands do not count as foreground processes for the purposes of this built-in command - i.e., status should ignore built-in commands.
-	printf("exit value %d", statusValue);
+	printf("exit value %d\n", statusValue);
 	fflush(stdout);
 }
 
